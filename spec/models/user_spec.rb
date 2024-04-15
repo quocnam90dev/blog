2d195692bcfe065d7e7812890aaddcc610bfa899
naka <quocnam90developer@gmail.com>
2024-04-15 22:41:04 +0700

    add factory_bot_rails,shoulda

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) {build(:user)}
+
+  it 'has a valid factory' do
+    expect(user).to be_valid
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
+end
